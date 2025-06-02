bfcb12c9d95e028ddbd74e0ba3b3f8eec08000fe
Anirudh3232 <166682718+Anirudh3232@users.noreply.github.com>
2025-06-02 11:55:24 -0500

    Update export_coordination_graph.py

--- scripts/export_coordination_graph.py
+++ scripts/export_coordination_graph.py
@@ -8,4 +8,4 @@ G.remove_nodes_from(list(nx.isolates(G)))
 
 
 nx.write_gml(G, "data/coordination_graph.gml")
-print("✅ Coordination graph exported to 'data/coordination_graph.gml'")
+print("Coordination graph exported to 'data/coordination_graph.gml'")
